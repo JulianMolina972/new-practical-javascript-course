7caa4959651c76670325691ac99e056a5738e778
julianmolina99 <juliandavidmv@ufps.edu.co>
2022-08-11 19:12:44 -0500

    top 10 salaries

--- analisis.js
+++ analisis.js
@@ -1,4 +1,4 @@
-//Analisís personal para Juanita
+//Analisís persona
 
 console.log(salarios)
 
@@ -9,13 +9,10 @@ function encontrarPersona (personaName) {
 
 function medianaPorpersona(nombrePerosna) {
   const trabajos = encontrarPersona(nombrePerosna).trabajos
-  console.log(trabajos)
   const salariosPersona = trabajos.map(element => element.salario)
-  console.log(salariosPersona)
 
   const medianaSalarios = PlatziMath.calcularMediana(salariosPersona)
 
-  console.log(medianaSalarios)
   return medianaSalarios
 }
 
@@ -79,7 +76,63 @@ function medianaSalariosYear(empresa, year) {
 
 console.log(empresas)
 
-function proyeccionPorEmpresa() {
+function proyeccionPorEmpresa(nombreEmpresa) {
+  if(!empresas[nombreEmpresa]) {
+    console.warn('La empresa no existe')
+  } else {
+    const empresasYears = Object.keys(empresas[nombreEmpresa])
+    const listaMedianaYears = empresasYears.map((year) => {
+      return medianaSalariosYear(nombreEmpresa, year)
+    });
+
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i < listaMedianaYears.length; i++) {
+      const salarioActual = listaMedianaYears[i]
+      const salarioAnterior = listaMedianaYears[i - 1]
+      const crecimiento = salarioActual - salarioAnterior
+      const porcentajeCrecimiento = crecimiento / salarioAnterior
+      porcentajesCrecimiento.push(porcentajeCrecimiento)
+      
+    }
+    
+    const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento)
+    
+      
+    const ultimaMediana = listaMedianaYears[listaMedianaYears.length - 1]
+    const aumento = ultimaMediana * medianaPorcentajesCrecimiento
+    const nuevamediana = aumento + ultimaMediana
+
+
+    console.log({nuevamediana})
+
+    return nuevamediana;
+
+  }
 
 }
 
+
+//Análisis general
+
+function medianaGeneral() {
+  const listaMedianas = salarios.map(persona => medianaPorpersona(persona.name));
+  const mediana = PlatziMath.calcularMediana(listaMedianas)
+  return mediana;
+}
+
+//Análisis top 10%
+
+function medianaTop10() {
+  const listaMedianas = salarios.map(persona => medianaPorpersona(persona.name));
+  const medianasOrdenadas = PlatziMath.ordenarLista(listaMedianas)
+  const top10 =  medianasOrdenadas.slice( -(medianasOrdenadas.length * 0.1))
+
+  const medianaTop10 = PlatziMath.calcularMediana(top10)
+
+  return medianaTop10;
+
+  console.log({medianasOrdenadas, top10})
+
+
+}
